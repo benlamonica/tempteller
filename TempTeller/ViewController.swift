da1935bf901dfd871d2eb0c16b275b05ed7995e8
Ben La Monica <ben.lamonica@gmail.com>
2015-05-19 08:33:17 -0500

    Initial Commit

---
+++ TempTeller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TempTeller
+//
+//  Created by Ben La Monica on 5/19/15.
+//  Copyright (c) 2015 Benjamin Alan La Monica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
