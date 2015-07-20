2cb83bc9dff9f9d36e311d1dd955fd8ebbbff790
Ben La Monica <ben.lamonica@gmail.com>
2015-07-20 17:22:42 -0500

    fixes #12

---
+++ TempTeller/ui/cells/HumidityCell.swift
@@ -0,0 +1,31 @@
+//
+//  HumidityCell.swift
+//  TempTeller
+//
+//  Created by Ben La Monica on 7/20/15.
+//  Copyright (c) 2015 Benjamin Alan La Monica. All rights reserved.
+//
+
+import UIKit
+
+class HumidityCell : UITableViewCell, SubRuleDisplaying {
+    @IBOutlet var humidity : UITextField!
+    @IBOutlet var opButton : UIButton!
+    
+    var subrule : HumiditySubRule?
+    
+    func displayRule(subrule : SubRule) {
+        if let rule = subrule as? HumiditySubRule {
+            self.subrule = rule
+            humidity.text = String("\(rule.value)")
+            opButton.titleLabel?.text = rule.op.rawValue
+        }
+    }
+    
+    func saveRule() {
+        if let rule = subrule {
+            rule.value = humidity.text.toDouble()
+            rule.op = CompOp(rawValue: opButton.titleLabel!.text!)!
+        }
+    }
+}
