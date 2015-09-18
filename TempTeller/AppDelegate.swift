bf501773fd5fcffaec142a407337d576350a0301
Ben La Monica <ben.lamonica@gmail.com>
2015-09-18 08:06:50 -0500

    fixes #33 - if app receives a heartbeat silent message, then it will reschedule a local notification to notify user if it doesn't receive a heartbeat within 15 minutes

--- TempTeller/AppDelegate.swift
+++ TempTeller/AppDelegate.swift
@@ -23,7 +23,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
+    
+    func scheduleHeartbeatLocalNotification(userInfo: [NSObject : AnyObject]) {
+        if userInfo["type"] as? String == "heartbeat" {
+            let prevNotifications = UIApplication.sharedApplication().scheduledLocalNotifications.filter {
+                if let notif = $0 as? UILocalNotification {
+                    return notif.alertTitle == "Missed Heartbeat"
+                } else {
+                    return false
+                }
+            }
+        
+            for notification in prevNotifications {
+                if let localNotif = notification as? UILocalNotification {
+                    UIApplication.sharedApplication().cancelLocalNotification(localNotif)
+                }
+            }
+            
+            let hbNotifier = UILocalNotification()
+            hbNotifier.alertTitle = "Missed Heartbeat"
+            hbNotifier.alertBody = "Haven't heard a heartbeat since \(NSDate())"
+            hbNotifier.fireDate = NSDate(timeIntervalSinceNow: 15.0 * 60)
+            hbNotifier.userInfo = userInfo
+            UIApplication.sharedApplication().scheduleLocalNotification(hbNotifier)
+        }
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo:  [NSObject : AnyObject]) {
+        scheduleHeartbeatLocalNotification(userInfo)
+    }
+    
     func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
         let token = deviceToken.base64EncodedStringWithOptions(nil)
         NSLog("registered for remote notifications: \(token)")
