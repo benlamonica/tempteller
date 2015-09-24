5b5ceee2baf5e0f2a17d4a9799de075b685e9c45
Ben La Monica <ben.lamonica@gmail.com>
2015-09-23 23:04:26 -0500

    working on the settings page

--- TempTeller/ui/SettingsViewController.swift
+++ TempTeller/ui/SettingsViewController.swift
@@ -13,7 +13,32 @@ class SettingsViewController : UIViewController {
     var nav : UINavigationController!
     @IBOutlet var serverUrl : UITextField!
     @IBOutlet var deviceToken : UITextField!
+    @IBOutlet var scrollView : UIScrollView!
+    @IBOutlet var contentView : UIView!
+    @IBOutlet var widthConstraint : NSLayoutConstraint!
+    
     let settings = NSUserDefaults.standardUserDefaults()
+    
+    @IBAction
+    func openForecastIO() {
+        UIApplication.sharedApplication().openURL(NSURL(string:"https://forecast.io")!)
+    }
+    
+    @IBAction
+    func openYahoo() {
+        UIApplication.sharedApplication().openURL(NSURL(string:"https://yahoo.com")!)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        let windows = UIApplication.sharedApplication().windows
+        let width = windows[0].frame.width
+        widthConstraint.constant = width
+        contentView.frame = CGRect(x: 0, y: 0, width: width, height: contentView.frame.height)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        scrollView.contentSize = CGSize(width: contentView.frame.width, height: contentView.frame.height)
+    }
     
     override func viewDidLoad() {
         if let url = settings.valueForKey("serverUrl") as? String {
